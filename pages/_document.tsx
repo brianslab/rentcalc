a27e1659e934390b5ebf408a78cc3d3e62c0b6d2
brianslab <bdkuhn602@gmail.com>
2023-01-02 02:30:01 -0800

    make tab title global

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,21 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang='en'>
+      <Head>
+        <title>Rent Calculator</title>
+        <meta
+          name='description'
+          content='Calculate the rent split for your household'
+        />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -5,15 +5,6 @@ import HouseholdShow from '../components/HouseholdShow';
 export default function Home() {
   return (
     <>
-      <Head>
-        <title>Rent Calculator</title>
-        <meta
-          name='description'
-          content='Calculate the rent split for your household'
-        />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
       <h1>RentCalc</h1>
       <HouseholdForm />
       <HouseholdShow />
