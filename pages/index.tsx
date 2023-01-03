ff00e2593875957cf3185743fe563f0fcb9a5bbb
brianslab <bdkuhn602@gmail.com>
2023-01-02 16:29:45 -0800

    fix yarn warning with head component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,20 @@
 import Link from 'next/link';
+import Head from 'next/head';
 import HouseholdForm from '../components/HouseholdForm';
 import HouseholdShow from '../components/HouseholdShow';
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>Rent Calculator</title>
+        <meta
+          name='description'
+          content='Calculate the rent split for your household'
+        />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.ico' />
+      </Head>
       <h1>RentCalc</h1>
       <HouseholdForm />
       <HouseholdShow />
