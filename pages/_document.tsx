bcc27044d8f83ce3880d4facaf00859703492545
brianslab <bdkuhn602@gmail.com>
2023-01-02 16:29:45 -0800

    fix yarn warning with head component

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,15 +3,7 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang='en'>
-      <Head>
-        <title>Rent Calculator</title>
-        <meta
-          name='description'
-          content='Calculate the rent split for your household'
-        />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
-        <link rel='icon' href='/favicon.ico' />
-      </Head>
+      <Head />
       <body>
         <Main />
         <NextScript />
