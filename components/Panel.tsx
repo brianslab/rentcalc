f2b2a3b88f3fb24766000b8a0427827e5ad58d3e
brianslab <bdkuhn602@gmail.com>
2023-01-02 16:27:21 -0800

    fix typing

--- components/Panel.tsx
+++ components/Panel.tsx
@@ -1,6 +1,7 @@
 import classNames from 'classnames';
+import { PanelProps } from './PanelTypes';
 
-function Panel({ children, className, ...rest }) {
+function Panel({ children, className, ...rest }: PanelProps) {
   const finalClassNames = classNames(
     'border rounded p-3 shadow bg-white w-full',
     className
